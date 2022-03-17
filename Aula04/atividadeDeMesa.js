71578bb33d1b9afbdc459ba00507654cd9bd79fd
Patricio <89046592+patriciogsilva@users.noreply.github.com>
2022-03-16 22:54:55 -0300

    Add files via upload

---
+++ Aula04/atividadeDeMesa.js
@@ -0,0 +1,8 @@
+var nome = "Patricio";
+var idade = 33;
+var peso = 66;
+var altura = 1.75;
+var plano = true;
+var imc = peso / (altura * altura) //dessa forma vai calcular primeiro o que está entre parenteses depois somar com o peso.
+
+console.log (nome + " tem " + idade + " anos e seu indice de massa corporal é: " + imc);
---
+++ Aula04/aula04.js
@@ -0,0 +1,21 @@
+console.log("Olá mundo!");
+
+
+// Isso é uma variavel.
+var x = 8;
+var y = 5;
+const z = 25; //tipo constante - não pode ser alterado o seu valor, diferente do var.
+let h = 96; // semelhante ao var e pode também ter seu valor alterado.
+
+let nome = "Patricio" //Variavel do tipo string
+let plano = true; // tipo booleano (true ou false)(Verdadeiro ou falso)
+
+let soma = h + x;
+let sub = h - x;
+let mult = h * x;
+let div = h / x;
+
+// Atribui outro valor a variavel.
+x = 5;
+
+console.log(nome, plano, soma, sub, mult, div);
